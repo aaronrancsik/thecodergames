ed27d032df8ad1e4ddd75d78eb7d3e38abcc213b
Aaron <makerstudio.info@gmail.com>
2019-02-21 13:41:47 +0100

    add ts loader for game

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,8 @@ module.exports = {
   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    'nuxt-typescript'
   ],
   /*
   ** Axios module configuration
