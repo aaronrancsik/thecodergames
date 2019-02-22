ad09545a68dcfe0eb11205414edc6a4592db4643
Aaron <makerstudio.info@gmail.com>
2019-02-22 13:57:47 +0100

    add game to page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,34 +2,18 @@
   <section class="container">
     <div>
       <logo />
-      <h1 class="title">
-        client
-      </h1>
-      <h2 class="subtitle">
-        My outstanding Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      <game /> 
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Logo from '~/components/Logo.vue';
+ import Game from '~/components/Game';
 export default {
   components: {
-    Logo
+    Logo,
+     Game
   }
 }
 </script>
