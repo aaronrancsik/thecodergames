f52d421f6bcfcdebf19585dec6a477ebe4342ee5
Aaron <makerstudio.info@gmail.com>
2019-02-22 13:57:36 +0100

    add game  component

---
+++ client/components/Game.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div id="gameid" v-if="downloaded" />
+        <div class="placeholder"  v-else >
+            Downloading...
+        </div>
+    </div>
+    
+    
+</template>
+<script>
+export default {
+    name: 'Game',
+    data(){
+        return {
+            downloaded:false
+        }
+    },
+    mounted(){
+        import('../game/game').then(game =>{
+            this.downloaded =true;
+            this.$nextTick(()=>game.launch());
+        })
+    }
+}
+</script>
