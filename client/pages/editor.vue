70823c3c56ea805433a60305890cf3980e908dc3
Aaron <makerstudio.info@gmail.com>
2019-02-25 18:42:46 +0100

    create editor page with Editor comp

---
+++ client/pages/editor.vue
@@ -0,0 +1,27 @@
+<template>
+    <span>
+        <BlocklyEditor />
+    </span>
+    
+    
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Game from "~/components/Game.vue";
+import BlocklyEditor from "~/components/BlocklyEditor.vue";
+
+@Component({
+    components:{
+        Game,
+        BlocklyEditor
+    }
+})
+export default class Editor extends Vue{
+
+}
+</script>
+
+<style>
+
+</style>
