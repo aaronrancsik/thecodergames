f5ed35df41e4cdb0324f4034889ac787bd5421c0
Aaron <makerstudio.info@gmail.com>
2019-03-07 19:02:25 +0100

    subscribe to global eventbus emmit comes from Blockly

--- client/components/Game.vue
+++ client/components/Game.vue
@@ -16,6 +16,24 @@ import {
 
 @Component
 export default class Game extends Vue  {
+    gameInst
+    created(){
+         // $on method will receive the updated count value from the sender component
+        this.$nuxt.$on('STEP_FORWARD', data => {
+            //alert("STEP_FORWARD"+data);
+            this.gameInst.alma();
+        });
+        this.$nuxt.$on('STEP_BACK', data => {
+            alert("STEP_BACK"+data);
+        });
+        this.$nuxt.$on('TURN_LEFT', data => {
+            alert("TURN_LEFT"+data);
+        });
+        this.$nuxt.$on('TURN_RIGHT', data => {
+            alert("TURN_RIGHT"+data);
+        });
+    }
+
     @Provide() downloaded:boolean =false;
 
     mounted(){
