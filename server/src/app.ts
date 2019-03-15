d78ff315390f1d35ba1151f358f6972ff2f3c95a
Aaron <makerstudio.info@gmail.com>
2019-03-15 19:26:29 +0100

    dev test

--- server/src/app.ts
+++ server/src/app.ts
@@ -59,7 +59,7 @@ export class App {
         });
 
         this.app.get('/', (req, res) => {
-            return res.send('Received a GET HTTP method ok 2');
+            return res.send('Received a GET HTTP method ok 3');
         });
 
         this.io.use(function(socket, next){
