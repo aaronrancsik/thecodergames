1fc6f66fd1d3addc0cb8032eaf3ecaffe5a3716e
Aaron <makerstudio.info@gmail.com>
2019-03-17 16:59:48 +0100

    create initial user router

---
+++ server/src/routes/api-router.ts
@@ -0,0 +1,34 @@
+import {Request, Response} from "express";
+import { UserController } from "../controllers/userController";
+
+
+export class Routes {
+    public userController: UserController = new UserController();
+    
+
+    public routes(app, test): void {          
+        app.route('/').get((req: Request, res: Response) => {            
+            res.status(200).send({
+                status: 'API Its Working',
+                message: 'Welcome to the jungle!',
+                test: test
+            })
+        })      
+        
+        // Contact 
+        app.route('/user') 
+        // GET endpoint 
+        .get(this.userController.getUsers)        
+        // POST endpoint
+        .post(this.userController.addNewUser)
+
+        // Contact detail
+        app.route('/user/:userId')
+        // get specific user
+        .get(this.userController.getUserWithID)
+        .put(this.userController.updateUser)
+        .delete(this.userController.deleteUser)
+    }
+}
+
+
