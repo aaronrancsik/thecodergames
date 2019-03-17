2e517a230e3de0652230be6d3156c7bd69d219e0
Aaron <makerstudio.info@gmail.com>
2019-03-17 16:57:18 +0100

    add  body parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import { createServer, Server } from 'http';
 import * as express from 'express';
 import * as socketIo from 'socket.io';
+import * as bodyParser from "body-parser";
 
 export class App {
     public static readonly PORT:number = 4455 || Number(process.env.PORT);
@@ -29,6 +30,8 @@ export class App {
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({ extended: false }));
 
                 }
 
