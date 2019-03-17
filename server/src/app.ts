e89f4e9f7223a8dfd393f648d316c42e336b6393
Aaron <makerstudio.info@gmail.com>
2019-03-17 17:00:08 +0100

    Use user controller

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import { createServer, Server } from 'http';
 import * as express from 'express';
 import * as socketIo from 'socket.io';
 import * as bodyParser from "body-parser";
+import { Routes } from "./routes/api-router";
 import * as mongoose from "mongoose";
 
 export class App {
@@ -10,18 +11,22 @@ export class App {
     private server: Server;
     private io: SocketIO.Server;
     private port: string | number;
+    public routePrv: Routes = new Routes();
     public mongoUrl: string = process.env.CUSTOMCONNSTR_Mongo;
 
     constructor() {
         this.createApp();
         this.config();
         this.createServer();
+        this.AddRoutes();
         this.sockets();
         this.listen();
     }
 
+   
+
     private createApp(): void {
-        this.app = express();
+        this.app = express(); 
     }
 
     private config(): void {
@@ -35,26 +40,26 @@ export class App {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
 
-                }
+    }
 
     private createServer(): void {
         this.server = createServer(this.app);
-            }    
+    }
     private AddRoutes(){
         this.routePrv.routes(this.app, this.mongoUrl);
-                    }
+    }
     private sockets(): void {
         this.io = socketIo(this.server);
-                }
+    }
     private listen(): void {
         this.server.listen(this.port, () => {
             console.log('Running server on port %s', this.port);
-            });
-                    }
+        });
+    }
     private mongoSetup(): void{
         (<any>mongoose).Promise = global.Promise;
         mongoose.connect(this.mongoUrl);    
-                }
+    }
     public getApp(): express.Application {
         return this.app;
     }
