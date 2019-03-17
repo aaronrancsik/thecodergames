48d6b3f398e1336e8c739e0a627cc6bab1ff961f
Aaron <makerstudio.info@gmail.com>
2019-03-17 23:25:02 +0100

    update user schema with level and some fix

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,19 +1,29 @@
 import * as mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+import  {LevelSchema} from './levelModel';
 
 export const UserSchema = new Schema({
-    name: {
+    username: {
         type: String,
-        required: 'Enter your name'
+        required: 'Name is required'
     },
     email: {
+        required:'Email is required',
         type: String            
     },
     school: {
+        required:'School is required',
         type: String 
     },
     created_date: {
         type: Date,
         default: Date.now
+    },
+    password:{
+        required:'Password is required',
+        type:String
+    },
+    levels:{
+        type: [LevelSchema]
     }
 });
