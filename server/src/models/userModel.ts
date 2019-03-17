4fd3c730340497b5832ec363db08a624c712f97e
Aaron <makerstudio.info@gmail.com>
2019-03-17 16:59:11 +0100

    create user modell

---
+++ server/src/models/userModel.ts
@@ -0,0 +1,19 @@
+import * as mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Enter your name'
+    },
+    email: {
+        type: String            
+    },
+    school: {
+        type: String            
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
