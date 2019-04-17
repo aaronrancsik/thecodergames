62073c6d823647ff355bfbc73a5d8659cb5153a8
Aaron <makerstudio.info@gmail.com>
2019-04-18 00:10:18 +0200

    create  initial gamebase component

---
+++ client/components/GameBase.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="sec">
+        <div id="gameid" v-if="downloaded" />
+        <div class="placeholder"  v-else >
+            Downloading...
+        </div>
+    </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue,
+  Provide
+} from "vue-property-decorator";
+
+@Component
+export default class GameBase extends Vue  {
+    gameInst
+    created(){
+        //  // $on method will receive the updated count value from the sender component
+        // this.$nuxt.$on('STEP_FORWARD', data => {
+        //     //alert("STEP_FORWARD"+data);
+        //     this.gameInst.moveForward(data);
+        // });
+        // this.$nuxt.$on('STEP_BACK', data => {
+        //    this.gameInst.moveBackward(data);
+        // });
+        // this.$nuxt.$on('TURN_LEFT', data => {
+        //      this.gameInst.turnLeft(data);
+        // });
+        // this.$nuxt.$on('TURN_RIGHT', data => {
+        //     this.gameInst.turnRight(data);
+        // });
+    }
+
+    @Provide() downloaded:boolean =false;
+
+    mounted(){
+        import('../game/game').then(game =>{
+            this.downloaded =true;
+            
+            this.$nextTick(()=>{
+                game.launch();
+                this.gameInst =game.getGame();
+            });
+
+            
+        });
+    }
+}
+</script>
+<style scoped>
+#gameid{
+    height: 100%;
+}
+.sec{
+    height: 100%;
+}
+</style>
