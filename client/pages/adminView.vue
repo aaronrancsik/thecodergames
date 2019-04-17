bcbf600b05c40649b9aba84636fb6a3712b97477
Aaron <makerstudio.info@gmail.com>
2019-04-18 00:11:29 +0200

    initial admin view page

---
+++ client/pages/adminView.vue
@@ -0,0 +1,15 @@
+<template>
+    <AdminGame />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import AdminGame from '~/components/AdminGame.vue';
+
+@Component({
+  components: {
+    AdminGame
+  }
+})
+export default class AdminView extends Vue {}
+</script>
