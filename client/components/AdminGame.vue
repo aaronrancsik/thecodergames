686bc5c208781faf94b88defbc0a77999dc7639d
Aaron <makerstudio.info@gmail.com>
2019-04-18 00:10:39 +0200

    initial AdminGame from GameBase

---
+++ client/components/AdminGame.vue
@@ -0,0 +1,24 @@
+<template>
+    <GameBase />
+</template>
+
+
+<script lang="ts">
+import {
+  Component,
+  Vue,
+  Provide
+} from "vue-property-decorator";
+import GameBase from '~/components/GameBase.vue';
+@Component({
+  components: {
+    GameBase
+  }
+})
+export default class AdminGame extends Vue  {
+
+}
+</script>
+<style scoped>
+
+</style>
