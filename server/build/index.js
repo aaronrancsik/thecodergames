4fe791400c719804bd81d7339790ce2bddeb5864
Aaron <makerstudio.info@gmail.com>
2019-04-20 00:12:07 +0200

    just build unfortunately azure not work withut this file :(

--- server/build/index.js
+++ server/build/index.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
 let app = new app_1.App().getApp();
 exports.app = app;
+//# sourceMappingURL=index.js.map
