c00efcfe8cbbfcf2fdcbd34ee9d5713ba54e53dc
Aaron <makerstudio.info@gmail.com>
2019-04-20 00:07:47 +0200

    create toolbar compi

---
+++ client/components/AccountManage.vue
@@ -0,0 +1,25 @@
+<template>
+ <v-toolbar light dense>
+      <v-toolbar-title >Szerkersztő panel</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat @click="logout">Kilépés</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+const Cookie = process.client ? require('js-cookie') : undefined
+@Component
+export default class AccountManage extends Vue {
+    logout() {
+      //todo add server call
+      Cookie.remove('auth');
+      this.$store.commit('setAuth', null);
+      location.reload(true);
+    }
+}
+</script>
+
+
