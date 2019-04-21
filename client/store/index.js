0cf60626af233b926ff3a4c7f9a87088568aa549
Aaron <makerstudio.info@gmail.com>
2019-04-21 14:27:35 +0200

    update from classic store

--- client/store/index.js
+++ client/store/index.js
@@ -2,31 +2,27 @@ import Vuex from 'vuex'
 
 const cookieparser = process.server ? require('cookieparser') : undefined
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      auth: null
-    },
-    mutations: {
-      setAuth(state, auth) {
-        state.auth = auth
-      }
-    },
-    actions: {
-      nuxtServerInit({ commit }, { req }) {
-        let auth = null
-        if (req.headers.cookie) {
-          const parsed = cookieparser.parse(req.headers.cookie)
-          try {
-            auth = JSON.parse(parsed.auth)
-          } catch (err) {
-            // No valid cookie found
-          }
-        }
-        commit('setAuth', auth)
-      }
-    }
-  })
+export const state=()=>{
+  auth:null
 }
 
-export default createStore
+export const mutations ={
+  setAuth(state, auth) {
+    state.auth = auth
+  }
+}
+
+export const actions ={
+  nuxtServerInit({ commit }, { req }) {
+    let auth = null
+    if (req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      try {
+        auth = JSON.parse(parsed.auth)
+      } catch (err) {
+        // No valid cookie found
+      }
+    }
+    commit('setAuth', auth)
+  }
+}
