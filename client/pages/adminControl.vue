a76fba19c5ceea3e6dad044d7e746aaf04983150
Aaron <makerstudio.info@gmail.com>
2019-04-22 21:49:47 +0200

    admin control comp

---
+++ client/pages/adminControl.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+    middleware:[
+      'authenticated'
+    ]
+})
+export default class AdminControl extends Vue {
+    mounted() {
+        console.log(window['$nuxt']._route.params);
+        window.open("/adminView","","status");
+     }
+}
+</script>
