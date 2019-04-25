728c6858fa3c75d7bf71c84bcf2348e4b7a29f36
Aaron <makerstudio.info@gmail.com>
2019-04-25 16:31:29 +0200

    create landing page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,22 +1,15 @@
 <template>
-<Game/>
+  <HelloWorld />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '~/components/HelloWorld.vue';
-import Game from '~/components/Game.vue'
 
 @Component({
   components: {
-    HelloWorld,
-    Game
+    HelloWorld
   }
 })
 export default class Home extends Vue {}
 </script>
-
-<style>
-</style>
-
-
